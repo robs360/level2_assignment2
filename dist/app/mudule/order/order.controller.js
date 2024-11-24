7c3f6e16c2c55c362d593bf5754853ffe45d521e
shahadat <nayemshahadat581@gmail.com>
2024-11-24 10:20:36 +0600

    order logic updated

--- dist/app/mudule/order/order.controller.js
+++ dist/app/mudule/order/order.controller.js
@@ -16,22 +16,35 @@ const product_model_1 = require("../product/product.model");
 const order_model_1 = require("./order.model");
 const createOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        let custumerOrder = '';
         let mess = "Order Not Possible";
         const order = req.body.order;
         const decrement = order.quantity;
         const query = { _id: new mongodb_1.ObjectId(order.product) };
         const Result2 = yield product_model_1.ProductModel.find(query);
         const update = { $inc: { quantity: -decrement } };
+        const update2 = {
+            $inc: { quantity: -decrement },
+            $set: { inStock: false }
+        };
         const options = { new: true };
-        if (Result2[0].quantity < decrement) {
+        if (Result2[0].quantity < decrement || !Result2[0].inStock) {
             res.status(404).json({
-                message: "Sorry Quantity Are Not Avialable"
+                message: "Sorry, the requested quantity is not available."
             });
         }
         else {
-            const result = yield order_services_1.OrderServices.createOrderIntoDB(order);
-            const updatedProduct = yield product_model_1.ProductModel.updateOne(query, update, options);
-            mess = result;
+            if (Result2[0].quantity === decrement) {
+                const result = yield order_services_1.OrderServices.createOrderIntoDB(order);
+                const updatedProduct = yield product_model_1.ProductModel.updateOne(query, update2, options);
+                custumerOrder = result;
+            }
+            else {
+                const result = yield order_services_1.OrderServices.createOrderIntoDB(order);
+                const updatedProduct = yield product_model_1.ProductModel.updateOne(query, update, options);
+                custumerOrder = result;
+            }
+            mess = '';
         }
         const currentTime = new Date().toISOString();
         const updatedResult = yield product_model_1.ProductModel.find(query);
@@ -39,7 +52,7 @@ const createOrder = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
             res.status(200).json({
                 success: true,
                 message: "Order created successfully!",
-                data: mess,
+                data: custumerOrder,
                 orderTime: currentTime,
                 updatedQuantity: "Product Model Updated",
                 UpdatedData: updatedResult
--- src/app/mudule/order/order.controller.ts
+++ src/app/mudule/order/order.controller.ts
@@ -3,47 +3,65 @@ import { OrderServices } from "./order.services";
 import { ObjectId } from 'mongodb';
 import { ProductModel } from "../product/product.model";
 import { OrderModel } from "./order.model";
+import { TOrder } from "./order.interface";
+
 
 const createOrder = async (req: Request, res: Response) => {
     try {
-        let mess:any="Order Not Possible"
+        let custumerOrder: TOrder | '' = '';
+        let mess: any = "Order Not Possible"
         const order = req.body.order;
 
         const decrement = order.quantity;
 
-        const query = { _id: new ObjectId(order.product) };
-
+        const query = { _id: new ObjectId(order.product)};
+                                                           
         const Result2 = await ProductModel.find(query);
         const update = { $inc: { quantity: -decrement } };
-
+        const update2 = {
+            $inc: { quantity: -decrement },
+            $set: { inStock: false }
+        };
         const options = { new: true };
-        if (Result2[0].quantity < decrement) {
+        if (Result2[0].quantity < decrement || !Result2[0].inStock) {
             res.status(404).json({
-                message: "Sorry Quantity Are Not Avialable"
-            })
+                message: "Sorry, the requested quantity is not available."
+            });
         }
 
-        else{
-            const result = await OrderServices.createOrderIntoDB(order)
-            const updatedProduct = await ProductModel.updateOne(query, update, options);
-            mess=result;
-        }
 
+        else {
             
+            if (Result2[0].quantity === decrement) {
+                const result = await OrderServices.createOrderIntoDB(order)
+                const updatedProduct = await ProductModel.updateOne(query, update2, options);
+                custumerOrder=result;
+            }
+            else {
+                const result = await OrderServices.createOrderIntoDB(order)
+                const updatedProduct = await ProductModel.updateOne(query, update, options);
+                custumerOrder=result;
+            }
+
+            mess = '';
+
+        }
         const currentTime = new Date().toISOString();
 
         const updatedResult = await ProductModel.find(query);
 
-       if(mess!=="Order Not Possible"){
-        res.status(200).json({
-            success: true,
-            message: "Order created successfully!",
-            data:mess,
-            orderTime: currentTime,
-            updatedQuantity: "Product Model Updated",
-            UpdatedData: updatedResult
-        });
-       }
+
+
+        if (mess !== "Order Not Possible") {
+            res.status(200).json({
+                success: true,
+                message: "Order created successfully!",
+                data: custumerOrder,
+                orderTime: currentTime,
+                updatedQuantity: "Product Model Updated",
+                UpdatedData: updatedResult
+            });
+        }
     }
     catch (err) {
         res.status(500).json({
